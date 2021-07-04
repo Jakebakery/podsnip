945c97294512e4c955c04ad5a77fdae48440ac3e
LandonYSplit <78182823+LandonYSplit@users.noreply.github.com>
2021-07-04 21:01:18 +0100

    api post

--- podsnip/components/upload-snippet/upload-snippet.js
+++ podsnip/components/upload-snippet/upload-snippet.js
@@ -8,46 +8,45 @@ const UploadSnippet = () => {
     const [caption, setCaption] = useState("");
     const [selectedFile, setSelectedFile] = useState();
     const [isFilePicked, setIsFilePicked] = useState(false);
-    const snippetUrl = "";
-    /*
-    const storeSnippetFile = (fileUrl) => {
-        fetch('https://localhost:8080/upload-snippet/', {
-        method: 'POST',
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            file: fileUrl,
-            caption: caption,
-            podcastName: podcastName,
-            podcastLink: podcastLink,
-        })
-    });   
-    }*/
+    var snippetUrl = "";
+
 
     const uuidv4 = () => {
-        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-          var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-          return v.toString(16);
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+            var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
         });
-      }
+    }
 
     const handleSubmit = (evt) => {
         evt.preventDefault();
-        if(!selectedFile){
+        if (!selectedFile) {
             alert('Nothing was attached');
-        } else{
+        } else {
             const uuid = uuidv4();
             const meta = {
                 contentType: "audio/mpeg",
             }
             storage.ref(`snippets/${uuid}`).put(selectedFile, meta).then((snapshot) => {
                 snapshot.ref.getDownloadURL().then((downloadURL) => {
-                    snippetUrl = downloadUrl;
+                    
+                    fetch('https://localhost:3000/snippet', {
+                        method: 'POST',
+                        headers: {
+                            Accept: 'application/json',
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify({
+                            url: downloadURL,
+                            caption: caption,
+                            podcastName: podcastName,
+                            podcastLink: podcastLink,
+                        })
+                    });
+                        
                 });
             });
-        
+
 
             alert(`Submitting form ${podcastLink}`);
         }
@@ -65,10 +64,10 @@ const UploadSnippet = () => {
                     Upload Audio
                 </h1>
                 <form onSubmit={handleSubmit}>
-                    <input className={styles.input} 
-                            type="file" 
-                            name="file" 
-                            onChange={onFileChange} />
+                    <input className={styles.input}
+                        type="file"
+                        name="file"
+                        onChange={onFileChange} />
                     <label className={styles.label}>
                         Link to podcast
                         <input
